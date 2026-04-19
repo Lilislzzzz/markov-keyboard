175f2117ddb38393f3732f9858874a3af4ddcf17
camillebre9-png <camillebre9@gmail.com>
2026-04-19 22:18:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // Le dossier "public" de Vite sert les fichiers statiquement à la racine.
-  // On indique ici que notre dossier public est à la racine du projet.
   publicDir: 'public',
   server: {
     port: 5173,
